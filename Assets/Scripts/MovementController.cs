582c7a8c2ca6d34002b820290d10588481974b1c
Arron Tancil <AJTancil@student.fullsail.edu>
2021-12-01 11:28:37 -0500

    Update MovementController.cs
    added a private rigid body component to the movement script inside of an awake function

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MovementController : MonoBehaviour
 {
     public float moveSpeed = 0;
     public float Jumpheight = 4000;
 
+    private Rigidbody2D rb;
     private bool activeCharactercar = false;
     private bool activeCharacterkong = false;
     private bool activeCharacterkiller = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
