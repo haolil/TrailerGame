dd20dcc2c614bd1c1398fa94dc7c62c4657de342
Arron Tancil <AJTancil@student.fullsail.edu>
2021-12-01 11:43:59 -0500

    update Movement
    added processInputs and fixedUpdate functions as well as variables for vertical and horizontal movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public float moveSpeed = 0;
+    public float moveSpeed = 5;
     public float Jumpheight = 4000;
 
     private Rigidbody2D rb;
     private bool activeCharactercar = false;
     private bool activeCharacterkong = false;
     private bool activeCharacterkiller = false;
+    private float vertMovementdirection = 0;
+    private float horiMovementdirection = 0;
 
     private void Awake()
     {
+        //grab the current rigid body that the script is attached to
         rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
+        processInputs();
+       
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
         
+    }
+    private void processInputs()
+    {
+        horiMovementdirection = Input.GetAxis("Horizontal");
+        vertMovementdirection = Input.GetAxis("Vertical");
     }
 }
