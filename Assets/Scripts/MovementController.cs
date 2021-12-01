52f8d90f7be001ec79e3226447084e782d0e23b5
Arron Tancil <AJTancil@student.fullsail.edu>
2021-12-01 11:13:19 -0500

    Update MovementController.cs
    Added bools for determining active characters  and public variables for movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public float moveSpeed = 0;
+    public float Jumpheight = 4000;
+
+    private bool activeCharactercar = false;
+    private bool activeCharacterkong = false;
+    private bool activeCharacterkiller = false;
+
     // Start is called before the first frame update
     void Start()
     {
