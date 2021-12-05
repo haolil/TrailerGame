e8c05d45c3d03d3dda20ab9da9392d8e5480b061
rbeard7 <84631913+rbeard7@users.noreply.github.com>
2021-12-04 19:43:45 -0800

    Pushing Audio Files

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,12 +7,23 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame ()
     {
+        playClick();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        playClick();
         Debug.Log("QUIT");
         Application.Quit();
+    }
+
+    public void playClick()
+    {
+        FindObjectOfType<AudioManager>().Play("Menu_Click");
+    }
+    public void playBackClick()
+    {
+        FindObjectOfType<AudioManager>().Play("Menu_Back_Click");
     }
 }
