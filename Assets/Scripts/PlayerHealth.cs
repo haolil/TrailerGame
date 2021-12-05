b723410b5e102a7c23d48831d660a2109c91daee
Shiaw-Rong Lin <lin_rong2005@hotmail.com>
2021-12-05 18:21:16 -0500

    Sean's Stuff

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image currentHealth;
+    public float maxHealth;
+    public float playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealth.fillAmount = playerHealth/10;
+
+        if(playerHealth == 0)
+        {
+
+        }
+
+    }
+}
