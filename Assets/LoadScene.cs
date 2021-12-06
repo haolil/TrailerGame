044990cb9ffb2cff7ced8c7d12f27adef34efaba
Monte <lazzara.monte@gmail.com>
2021-12-06 02:34:36 -0500

    added new end scriptand buttons. Turned off raycast target for UI curtain

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public GameObject UIButton;
+    public GameObject curtain;
+    Animator anim;
+
+    public void Start()
+    {
+        anim = curtain.GetComponent<Animator>();
+    }
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void TheEnd()
+    {
+        anim.Play("CuratainClose");
+        UIButton.SetActive(true)
+;    }
+}
